81266e1a3e55f7043914de69800d68c02be89b1a
Ondřej Bukartyk <ondrej.bukartyk@gmail.com>
2022-02-06 14:18:34 +0100

    add component ViewVariantItem

---
+++ src/components/ViewVariantItem.tsx
@@ -0,0 +1,41 @@
+import { Text } from "@chakra-ui/react";
+import React, { useCallback, VFC } from "react";
+import styled from "styled-components";
+import { ViewVariant } from "../types";
+
+const ItemContainer = styled("div")`
+  display: flex;
+  user-select: none;
+  padding: 0.25rem;
+  :hover {
+    background-color: #e9f5fc;
+  }
+`;
+
+type ViewVariantItemProps = {
+  viewVariant: ViewVariant;
+  icon: JSX.Element;
+  active: boolean;
+  onClick: (variant: ViewVariant) => void;
+};
+
+const ViewVariantItem: VFC<ViewVariantItemProps> = ({
+  viewVariant,
+  icon,
+  active,
+  onClick,
+}) => {
+  const handleClick = useCallback(
+    () => onClick(viewVariant),
+    [viewVariant, onClick]
+  );
+
+  return (
+    <ItemContainer onClick={handleClick}>
+      {icon}
+      <Text color={active ? "#1379d3" : "#878a8c"}>{viewVariant}</Text>
+    </ItemContainer>
+  );
+};
+
+export default ViewVariantItem;
