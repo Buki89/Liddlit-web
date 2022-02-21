b8ed835e7bfe8790afc5e04c46835aa2f7707dcd
Ondřej Bukartyk <ondrej.bukartyk@gmail.com>
2022-02-21 22:45:56 +0100

    page user profile created

---
+++ src/pages/user-profile.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import React, { FC } from "react";
+
+const UserProfile: NextPage = () => {
+  return <>user profile work in progress</>;
+};
+
+export default UserProfile;
