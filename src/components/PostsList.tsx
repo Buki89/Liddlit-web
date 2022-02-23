29d3783b38ec8b900bf84335fba81aedd316c176
Ondřej Bukartyk <ondrej.bukartyk@gmail.com>
2022-02-23 12:58:50 +0100

    add fetch more button

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,24 +1,71 @@
-import { Stack } from "@chakra-ui/react";
-import React, { FC } from "react";
-import { MeQuery, Post } from "../generated/graphql";
+import { Button, Flex, Stack } from "@chakra-ui/react";
+import React, { FC, useCallback } from "react";
+import { MeQuery, useAllPostsQuery } from "../generated/graphql";
 import PostListItem from "./PostListItem";
 
 type PostsListProps = {
-  posts: Post[] | undefined;
   meData: MeQuery | undefined;
 };
 
-const PostsList: FC<PostsListProps> = ({ posts, meData }) => {
-  if (!posts) {
+const PostsList: FC<PostsListProps> = ({ meData }) => {
+  const { data, loading, fetchMore } = useAllPostsQuery({
+    variables: {
+      limit: 2,
+      offset: 0,
+    },
+
+    notifyOnNetworkStatusChange: true,
+  });
+
+  const posts = data?.allPosts?.posts;
+
+  const postsLength = data?.allPosts?.posts?.length;
+
+  const handleClick = useCallback(
+    (e) => {
+      e.preventDefault();
+      fetchMore({
+        variables: {
+          offset: postsLength,
+        },
+      });
+    },
+    [fetchMore, postsLength]
+  );
+
+  if (!posts && loading) {
+    return <p>loading</p>;
+  }
+
+  if (!posts && !loading) {
     return <p>no post yet</p>;
   }
 
   return (
-    <Stack spacing={4}>
-      {posts.map((post) => (
-        <PostListItem key={post.id} post={post} me={meData?.me} />
-      ))}
-    </Stack>
+    <>
+      <Stack spacing={4} mb={4}>
+        {posts?.map((post) => {
+          if (!post) {
+            return null;
+          }
+
+          return <PostListItem key={post.title} post={post} me={meData?.me} />;
+        })}
+      </Stack>
+      {data?.allPosts?.hasMore && (
+        <Flex>
+          <Button
+            onClick={handleClick}
+            isLoading={loading}
+            margin="auto "
+            my={8}
+            type="button"
+          >
+            load more
+          </Button>
+        </Flex>
+      )}
+    </>
   );
 };
 
