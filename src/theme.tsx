e42683e6c9059d47051df89836f008b05527c6af
Ondřej Bukartyk <ondrej.bukartyk@gmail.com>
2022-02-23 13:03:34 +0100

    add blue, lighter blue color to theme, change font family

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,7 +1,7 @@
 import { extendTheme } from "@chakra-ui/react";
 import { createBreakpoints } from "@chakra-ui/theme-tools";
 
-const fonts = { mono: `'Menlo', monospace` };
+const fonts = { mono: `'Verdana','Menlo', monospace` };
 
 const breakpoints = createBreakpoints({
   sm: "40em",
@@ -13,15 +13,18 @@ const breakpoints = createBreakpoints({
 const theme = extendTheme({
   colors: {
     black: "#16161D",
+    blue: "#1379d3",
+    lighterBlue: "#1884d6",
   },
   fonts,
   breakpoints,
+
   styles: {
     global: {
       body: {
         bg: "#dae0e6",
         color: "#000",
-        fontFamily: "Source Code Pro",
+        fontFamily: "Verdana",
       },
     },
   },
